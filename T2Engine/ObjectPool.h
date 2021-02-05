123f7ff0dcef5779a3d3eb8cecf6455e8286a095
Flieven <36849353+Flieven@users.noreply.github.com>
2021-02-05 15:08:50 +0100

    Comment cleanup
    Removed some comments not relevant to code.

--- T2Engine/ObjectPool.h
+++ T2Engine/ObjectPool.h
@@ -13,20 +13,6 @@ Result: std::map< [SearchTerm] , std::list< [ObjectPointers] >> call it "pool" (
 (*) Ability to clear pool if no longer in use ( void destroyPool(std::string type) ) <--- OVERLOAD WITH VERSION THAT KILLS ALL POOLS ON DECONSTRUCTION.
 
 NOTES:
-		This is essentially an Abstract Factory <--- GOOGLE IT, IT'S BEEN A WHILE SINCE WE USED IT!
-
-		Idea 1:
-		Best way to do this? Swtich that returns an Object* (each case being the constructor of specific object based on tag given?).
-		Idea 2:
-		Do proper Abstract Factory with Concrete Factories as well? Won't *really* need it though?
-		Idea 456:
-		Use GameConfig to access all available tags (they will be static) and then use that with a swtich and just pass the tag you need.
-		Idea 666:
-		Shoot yourself and don't have to worry about it anymore....
-		Should the tags be unique strings for each object in GameConfig, or a global enum?
-		Global enum seems best, let's do that...
-		But then we can't use the GameConfig because it sits in the Game not the Engine... \o/
-
 		NEW PROBABLY BETTER IDEA:
 		Create FactoryManager and Abstract Factory.
 
